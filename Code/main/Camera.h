8bf5fdf4e03673676065664049357ee7f6da258a
Evan Bhogal <106604572+esb8@users.noreply.github.com>
2025-09-24 14:44:23 -0400

    Fix: Camera.h

--- Code/main/Camera.h
+++ Code/main/Camera.h
@@ -1,8 +1,8 @@
 #define PWDN_GPIO_NUM  -1
 #define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM  15
-#define SIOD_GPIO_NUM  4
-#define SIOC_GPIO_NUM  5
+
+#define SIOD_GPIO_NUM  20
+#define SIOC_GPIO_NUM  3
 
 #define Y2_GPIO_NUM    11
 #define Y3_GPIO_NUM    9
@@ -16,6 +16,7 @@
 #define VSYNC_GPIO_NUM 6
 #define HREF_GPIO_NUM  7
 #define PCLK_GPIO_NUM  13
+#define XCLK_GPIO_NUM  15
 
 #pragma once
 void cameraSetup();
